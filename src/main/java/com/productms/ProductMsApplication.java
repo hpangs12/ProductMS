b3ab39560b8fc12b958da8ea455f895ec172b55a
Henry Pangsatabam <pangsatabamhenry@gmail.com>
2025-10-09 19:22:43 +0530

    discovery and gateway added

--- src/main/java/com/productms/ProductMsApplication.java
+++ src/main/java/com/productms/ProductMsApplication.java
@@ -3,11 +3,13 @@ package com.productms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin
 @EnableCaching
+@EnableDiscoveryClient
 public class ProductMsApplication {
 
 	public static void main(String[] args) {
